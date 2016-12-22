16e8c5ffda8ed931152342f4349460656dac775c
dtrop <dtrop@192.168.1.9>
2016-12-22 11:16:56 +0100

    Completata GA, DA TESTARE

--- src/storageLayer/DatabaseGA.java
+++ src/storageLayer/DatabaseGA.java
@@ -70,14 +70,59 @@ public ArrayList<Aula> RicercaAule(Giorno giorno,Time oraInizio,Time oraFine){
 	return listaAuleLibere;
 	
 }
-public static ArrayList<OraAula> visualizzaInfoAula(){
-	return null;
+public static ArrayList<OraAula> visualizzaInfoAula(String nome,Giorno giorno){
+	Connection connection = null;
+	PreparedStatement psInsertOraAula = null;
+	ArrayList<OraAula> listaOreLibere=new ArrayList<OraAula>();
+	try {
+		PreparedStatement psGetListaAule=connection.prepareStatement(queryGetListaAule);
+		psGetListaAule.setString(1, nome);
+		psGetListaAule.setString(2, giorno.toString());
+		ResultSet rs=psGetListaAule.executeQuery();
+		while (rs.next()) {
+			Giorno g=Giorno.valueOf(rs.getString("giorno"));
+			OraAula a=new OraAula(rs.getString("Nome"),g,rs.getTime("oraInizio"),rs.getTime("oraFine"),rs.getBoolean("defaultStatus"),rs.getBoolean("feedStatus"),rs.getString("emailUtente"));
+			listaOreLibere.add(a);
+		}
+		connection.commit();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}finally {
+		try {
+			Database.releaseConnection(connection);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	return listaOreLibere;
 	
 }
+public static boolean invioFeedback(String Nome,boolean status,String emailUtente){
+	Connection connection = null;
+	PreparedStatement psInvioFeedback = null;
+	
+	try {
+		connection=Database.getConnection();
+		psInvioFeedback=connection.prepareStatement(queryInvioFeedback);
+		
+		psInvioFeedback.setString(1, Nome);
+		psInvioFeedback.setBoolean(2, status);
+		psInvioFeedback.setString(3, emailUtente);
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return false;
+}
+
 	public static String queryAddOraAula;
 	public static String queryGetListaAule;
 	public static String queryRicercaAule;
 	public static String queryVisualizzaInfoAule;
+	public static String queryInvioFeedback;
 	static {
 		queryAddOraAula = "INSERT INTO `redteam`.`oraaula` (`Nome`, `giorno`, `oraInizio`, `oraFine`, `defaultStatus`, `feedStatus`, `emailUtente`) VALUES (?, ?, ?, ?, ?, ?, ?);";
 		queryGetListaAule="SELECT * FROM redteam.aula";
@@ -86,7 +131,10 @@ public static ArrayList<OraAula> visualizzaInfoAula(){
 				+ "WHERE oa.giorno =?  and oa.oraInizio >= ? and oa.oraFine <= ? and oa.defaultStatus = true and a.nome = oa.nome";
 		queryVisualizzaInfoAule="SELECT Nome, OraInizio, OraFine, defaultStatus, feedStatus, emailUtente"+
 								"FROM redteam.oraaula "+
-								"WHERE nome = ?  and giorno = ? and defaultStatus = ?";
+								"WHERE nome = ?  and giorno = ? and defaultStatus = true";
+		queryInvioFeedback="UPDATE redteam.oraaula "
+				+ "SET feedStatus = ?, emailUtente = ?"
+				+ "where nome = ? and giorno = ?;";
 	}
 
 }
