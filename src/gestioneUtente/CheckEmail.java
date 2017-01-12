98b7e1f01a280548b0ab04a7278c87684fb1b0f1
jerryae <a.esposito696@gmail.com>
2017-01-12 19:22:47 +0100

    added checkMail for from register

---
+++ src/gestioneUtente/CheckEmail.java
@@ -0,0 +1,58 @@
+package gestioneUtente;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import storageLayer.DatabaseGU;
+
+/**
+ * Servlet implementation class CheckEmail
+ */
+@WebServlet("/CheckEmail")
+public class CheckEmail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckEmail() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		
+		try
+		{
+			Utente u = DatabaseGU.getUtenteByID(email);
+			if(u!=null)
+			{
+				response.getWriter().print("{\"valid\" : false}");
+			}
+			else
+			{
+				response.getWriter().print("{\"valid\" : true}");
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
